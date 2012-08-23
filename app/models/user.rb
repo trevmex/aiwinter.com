187d5f0d8d1c31f36afd28845a31a38f63770eeb
Trevor Lalish-Menagh <trev@trevreport.org>
2012-08-22 23:07:29 -0400

    Fixed bad code in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  if Rails.env.production
+  if Rails.env.production?
     devise :database_authenticatable,
            :recoverable, :rememberable, :trackable, :validatable
   else
