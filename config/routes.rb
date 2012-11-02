324ea8bdb0833d6d4a0109302521791183dc0e3c
Dustin Getz <dustin.getz@gmail.com>
2012-11-01 22:45:07 -0400

    first pass at marketing copy

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ AiwinterCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'cities#index'
+  root :to => 'papers#index'
 
   # See how all your routes lay out with "rake routes"
 
